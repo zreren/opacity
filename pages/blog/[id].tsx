c400ffde662c039c670326118622a2f4a2a253b1
opacity <1152691418@qq.com>
2024-01-21 15:30:05 +0800

    update

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -59,7 +59,7 @@ export default function Article({ postData }) {
             li: Li,
             h4: H4,
             hr: Hr,
-            code({ node, inline, className, children, ...props }) {
+            code({ node, inline, className, children, ...props }:any) {
               const match = /language-(\w+)/.exec(className || '')
               return !inline && match ? (
                 <SyntaxHighlighter

fdcfd936f446a1499c6a1bd9d7243001fcd7206c
opacity <1152691418ms@gmail.com>
2025-01-08 00:40:32 +0800

    update

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -56,7 +56,10 @@ export default function Article({ postData }) {
           <meta name="twitter:title" content={postData.title} />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:description" content={postData.description} />
-          <meta name="twitter:image" content={postData?.interface} />
+          <meta
+            name="twitter:image"
+            content={postData?.interface || `https://og.zhaikr.com/api/v1/images?template=x:header-basic&title=${encodeURIComponent(postData.title)}&description=${encodeURIComponent(postData.description || '')}`}
+          />
         </Head>
         <Title>
           {postData.title} <Badge>{postData.dateYMD}</Badge>

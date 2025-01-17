76a7013d2a89155b1113b5d0e05c91d610a89586
opacity <1152691418ms@gmail.com>
2025-01-17 13:29:46 +0800

    update

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -71,18 +71,6 @@ export default function Article({ postData }) {
               name="twitter:image"
               content={`https://www.opacity.ink${postData?.interface}?v=${Math.random()}`}
             />
-            <Script
-              id="gtm-script"
-              strategy="afterInteractive"
-            >
-              {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5WX4K89V');
-        `}
-            </Script>
           </Head>
 
           <Title>
@@ -139,6 +127,7 @@ export default function Article({ postData }) {
         </noscript>
       )}
     </>
+
   )
 }
 // dynamic route IDs

fef0e2b77b9b22ff486b94fed2f50b77e5abe97b
opacity <1152691418ms@gmail.com>
2025-01-17 12:08:14 +0800

    update

--- components/layouts/article.tsx
+++ components/layouts/article.tsx
@@ -56,6 +56,12 @@ const Layout = ({ children, title, keywords, canonicalUrl, description, image })
 
             {/* Analytics */}
             <script async src="https://us.umami.is/script.js" data-website-id="3a211f2c-c381-4a60-848a-6833a7932f35"></script>
+            <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WX4K89V');</script>
+
           </Head>
         )}
         {children}
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -68,11 +68,6 @@ export default function Article({ postData }) {
             name="twitter:image"
             content={`https://www.opacity.ink${postData?.interface}?v=${Math.random()}`}
           />
-          <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5WX4K89V');</script>
         </Head>
 
         <Title>
